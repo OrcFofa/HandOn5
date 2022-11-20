9ba978793f8599283c2c34d74695b803af2ac254
Laura <devorcfofa@gmail.com>
2022-11-20 02:35:17 -0300

    Subindo o header pronto e modificdo + Favicon

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import "./Menu.css"
-import {Container} from 'reactstrap';
 import {Nav} from 'react-bootstrap';
 import {Navbar} from 'react-bootstrap';
 import logo from "../../assets/img/logo.png"
@@ -8,35 +6,27 @@ import tiktok from "../../assets/img/tiktok.png"
 import cart from "../../assets/img/cart.png"
 import insta from "../../assets/img/insta.png"
 import spotify from "../../assets/img/spotify.png"
+import "./Menu.css"
 
 function Menu ()  {
     return (
-        <Navbar className="" bg="dark" variant="dark">
-        <Container>
+        <Navbar className="menu">
           <Navbar.Brand href="#home">
-            <img
-              src={logo}
-              width="110"
-              height="50"
-              alt="Logo da Breja"
-            />
+            <img src={logo} alt="Logo da Breja"/>
           </Navbar.Brand>
-
-          <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Sobre</Nav.Link>
-            <Nav.Link href="#link2">Cervejas</Nav.Link>
-            <Nav.Link href="#link3"><img src={cart} width="25" height="25" alt="Imagem do carrinho de compras"/></Nav.Link>
-
+             
+          <Navbar.Collapse>
+            <Nav.Link className="linkes" href="#links">Home</Nav.Link>
+            <Nav.Link className="linkes" href="#link">Sobre</Nav.Link>
+            <Nav.Link className="linkes" href="#link2">Cervejas</Nav.Link>
+            <Nav.Link className="linkes" href="#link3">Carrinho<img src={cart} width="25" height="25" alt="Imagem do carrinho de compras"/></Nav.Link>
             
-
-            <Nav.Link href="#link4"><img src={tiktok} width="25" height="25" alt="Logo do TikTok"/></Nav.Link>
-            <Nav.Link href="#link5"><img src={insta} width="25" height="25" alt="Logo do Instagram"/></Nav.Link>
-            <Nav.Link href="#link6"><img src={spotify} width="25" height="25" alt="Logo do Spotify"/></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        </Container>
+            <Nav>
+            <Nav.Link id="barreira" className="icones" href="#link4"><img src={tiktok} width="25" height="25" alt="Logo do TikTok"/></Nav.Link>
+            <Nav.Link className="icones" href="#link5"><img src={insta} width="25" height="25" alt="Logo do Instagram"/></Nav.Link>
+            <Nav.Link className="icones" href="#link6"><img src={spotify} width="25" height="25" alt="Logo do Spotify"/></Nav.Link>
+            </Nav>
+        </Navbar.Collapse> 
       </Navbar>
     )
 }
