f689c08b3bdddc267f4d26d4ff99d8b52f116201
Laura <devorcfofa@gmail.com>
2022-11-23 20:30:55 -0300

    Subindo o Header e o Footer responsivos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,9 +12,9 @@ function Footer ()  {
         <Row>
             <Col className="feet">
                <p id="p1">Cervejaria Breja</p>
-               <p>Rua: São Judas N-320 , Bairro das Industrias ,Belo Horizonte -MG , CEP : 0000000</p>
-               <p><img className="imag" src={whats} width="25" height="25" alt="Logo do Whatsapp"/>(31) 00000-0000</p>
-               <p><img className="imag" src={phone} width="25" height="25" alt="Imagem de um telefone"/>(31) 00000-0000</p>
+               <p className='frase'>Rua: São Judas N-320 , Bairro das Industrias ,Belo Horizonte -MG , CEP : 0000000</p>
+               <p className='frase'><img className="imag" src={whats} width="25" height="25" alt="Logo do Whatsapp"/>(31) 00000-0000</p>
+               <p className='frase'><img className="imag" src={phone} width="25" height="25" alt="Imagem de um telefone"/>(31) 00000-0000</p>
             </Col>
         </Row>
      </Container>
