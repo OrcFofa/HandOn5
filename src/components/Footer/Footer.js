cad2df89693b7b26e2b9d0b5999e95b067721b5e
Laura <devorcfofa@gmail.com>
2022-11-22 21:51:39 -0300

    Subindo o Favicon correto + Header completo e responsivo + Caminho para o Carrinho e Spotify

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,8 +13,8 @@ function Footer ()  {
             <Col className="feet">
                <p id="p1">Cervejaria Breja</p>
                <p>Rua: São Judas N-320 , Bairro das Industrias ,Belo Horizonte -MG , CEP : 0000000</p>
-               <p><img src={whats} width="25" height="25" alt="Logo do Whatsapp"/>(31) 00000-0000</p>
-               <p><img src={phone} width="25" height="25" alt="Imagem de um telefone"/>(31) 00000-0000</p>
+               <p><img className="imag" src={whats} width="25" height="25" alt="Logo do Whatsapp"/>(31) 00000-0000</p>
+               <p><img className="imag" src={phone} width="25" height="25" alt="Imagem de um telefone"/>(31) 00000-0000</p>
             </Col>
         </Row>
      </Container>
--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -12,26 +12,25 @@ import insta from "../../assets/img/insta.png"
 import spotify from "../../assets/img/spotify.png"
 import "../Header/Menu.css"
 
-
 function Menu ()  {
     return (
-      <Container className="menu" fluid xs={1} md={5} lg={6}>
+      <Container className="menu" fluid >
         <Row>
-        <Navbar>
-          <Navbar.Brand href="#home">
-            <img src={logo} width="100" height="45" alt="Logo da Breja"/>
+        <Navbar expand="lg">
+          <Navbar.Brand href="/">
+            <img src={logo} id="ims" alt="Logo da Breja"/>
           </Navbar.Brand>
-             
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse >
-            <Nav.Link className="path" href="#links">Home</Nav.Link>
-            <Nav.Link className="path" href="#link">Sobre</Nav.Link>
-            <Nav.Link className="path" href="#link2">Cervejas </Nav.Link>
-            <Nav.Link className="path" href="../../pages/Cart">Carrinho<img src={cart} width="25" height="25" alt="Imagem do carrinho de compras"/><Badge id="badgenav" bg="">9</Badge></Nav.Link>
+            <Nav.Link id="colors" href="/">Home</Nav.Link>
+            <Nav.Link id="colors" href="#link">Sobre</Nav.Link>
+            <Nav.Link id="colors" href="#link2">Cervejas </Nav.Link>
+            <Nav.Link id="colors" href="/carrinho">Carrinho<img src={cart} id="ims2" alt="Imagem do carrinho de compras"/><Badge id="badgenav" bg="">9</Badge></Nav.Link>
             
             <Nav>
-            <Nav.Link id="barrier" className="icones" href="#link4"><img src={tiktok} width="25" height="25" alt="Logo do TikTok"/></Nav.Link>
-            <Nav.Link className="icones" href="#link5"><img src={insta} width="25" height="25" alt="Logo do Instagram"/></Nav.Link>
-            <Nav.Link className="icones" href="#link6"><img src={spotify} width="25" height="25" alt="Logo do Spotify"/></Nav.Link>
+            <Nav.Link href="#link4"><img src={tiktok} className="imgs" alt="Logo do TikTok"/></Nav.Link>
+            <Nav.Link href="#link5"><img src={insta} className="imgs" alt="Logo do Instagram"/></Nav.Link>
+            <Nav.Link href="https://open.spotify.com/playlist/0ksJT1bvv7iCcML6sffMJX?si=IF01bM4MR-W59AvnjrEfMw&utm_source=copy-link"><img src={spotify} className="imgs" alt="Logo do Spotify"/></Nav.Link>
             </Nav>
         </Navbar.Collapse> 
       </Navbar>
