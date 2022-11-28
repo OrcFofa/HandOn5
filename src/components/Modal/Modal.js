8626147899f20d7220a75dc7f91e74512b3ed77b
Jessica Arf <jessicalealarf8@gmail.com>
2022-11-27 22:46:05 -0300

    responsive style created on modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import Modal from "react-modal";
-import { useState } from "react";
+import {useState} from "react";
 import Logo from "../../assets/img/home-img/logo.png"
 import "./Modal.css"
 
@@ -7,33 +7,34 @@ import "./Modal.css"
 Modal.setAppElement('#root');
 
 
-export default function ModalOpen(){
-    
+export default function ModalOpen() {
+
     const [modalIsOpen, setIsOpen] = useState(true);
 
-    function maiorIdade(){
+    function maiorIdade() {
         setIsOpen(false);
     }
-  
-    return(
-      <>
-      <Modal
-      isOpen={modalIsOpen}
-      className="modal-idade"
-      contentLabel="Example Modal"
 
-      >
-        <div className="bgModal">
-        <div className="containerModal">
-        <img src={Logo} alt="logo"></img>
-        <h1>VOCÊ TEM MAIS DE 18 ANOS ?</h1>
-        <div>
-        <button onClick={maiorIdade} className="btnYes">SIM</button>
-        <a href="https://www.google.com.br/" className="btnNot">NÃO</a>
-        </div>
-        </div>
-        </div>
-      </Modal>
-      </>
+    return (
+        <>
+            <Modal isOpen={modalIsOpen}
+                className="modal-idade"
+                contentLabel="Example Modal">
+                <div className="bgModal">
+                    <div className="containerModal">
+                        <img src={Logo}
+                            alt="logo"></img>
+                        <h1>VOCÊ TEM MAIS DE 18 ANOS ?</h1>
+                        <div>
+                            <button onClick={maiorIdade}
+                                className="btnYes">SIM</button>
+                            <button className="btnNot">
+                                <a href="https://www.google.com.br/">NÃO</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+        </>
     )
 }
--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,25 +1,27 @@
-import React from "react";
 
 export default function Text() {
     return (
         <section className="container2">
 
-            <h2> Nós somos a diferença! </h2>
+            <h2>
+                Nós somos a diferença!
+            </h2>
 
             <div class="col-12 col-lg-6">
                 <div class="p-3">
                     <div className="text1">
-                    <span> Aqui é BREJA, cerveja jovem pra curtir com a galera !</span>
-                    <span>Somos uma cervejaria “boa demais da conta”, produzida em Belo Horizonte-MG por uma mineirada que faz só trem bão. Ingredientes de verdade, nada de água de batata e milho.</span>
-                    <span>Aqui tem: Malte,lúpulo e muito sabor.</span>
+                        <span>
+                            Aqui é BREJA, cerveja jovem pra curtir com a galera !</span>
+                        <span>Somos uma cervejaria “boa demais da conta”, produzida em Belo Horizonte-MG por uma mineirada que faz só trem bão. Ingredientes de verdade, nada de água de batata e milho.</span>
+                        <span>Aqui tem: Malte,lúpulo e muito sabor.</span>
+                    </div>
+                    <div className='text2'>
+                        <p>
+                            Cerveja de verdade que fala, né?
+                        </p>
+                    </div>
                 </div>
-            <div className='text2'>
-                <p>
-                Cerveja de verdade que fala, né?
-                </p>
-            </div></div>
             </div>
         </section>
     );
 }
-
